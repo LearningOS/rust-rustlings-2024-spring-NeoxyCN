eaecdd25199c9a8d4bd62f60800e976687d7c051
Neoxy <1007050740@qq.com>
2024-05-16 14:59:28 +0000

    iter

--- exercises/iterators/iterators3.rs
+++ exercises/iterators/iterators3.rs
@@ -26,12 +26,13 @@ pub struct NotDivisibleError {
 // Calculate `a` divided by `b` if `a` is evenly divisible by `b`.
 // Otherwise, return a suitable error.
 pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
-    
-    if(){
-        
+    if b == 0 {
+        return Err(DivisionError::DivideByZero);
     }
-    
-    Err()
+    if a % b != 0 {
+        return Err(DivisionError::NotDivisible(NotDivisibleError { dividend: a, divisor: b }));
+    }
+    Ok(a / b)
 }
 
 // Complete the function and return a value of the correct type so the test
